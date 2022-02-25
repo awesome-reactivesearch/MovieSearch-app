a9f9ac148acf4b56a70d458d41add96b74bbd0d0
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2022-02-25 10:49:59 +0530

    feat: add searchbox

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
 import {
   ReactiveBase,
-  DataSearch,
+  SearchBox,
   MultiDataList,
   RangeSlider,
   DateRange,
   MultiList,
   SingleRange,
+  SelectedFilters,
+  ReactiveList,
 } from "@appbaseio/reactivesearch";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isClicked: false,
+      message: "🔬Show Filters",
+    };
+  }
+
+  handleClick() {
+    this.setState({
+      isClicked: !this.state.isClicked,
+      message: this.state.isClicked ? "🔬 Show Filters" : "🎬 Show Movies",
+    });
+  }
+
   render() {
     return (
       <div className="main-container">
@@ -36,7 +54,6 @@ class App extends Component {
           }}
         >
           <div className="navbar">
-            {" "}
             <div className="logo-container">
               <img
                 className="app-logo"
@@ -44,8 +61,9 @@ class App extends Component {
                 alt="MovieSearch"
               />
             </div>
+
             <div className="search-container">
-              <DataSearch
+              <SearchBox
                 componentId="mainSearch"
                 dataField={["original_title"]}
                 categoryField="title"
@@ -53,14 +71,31 @@ class App extends Component {
                 queryFormat="and"
                 placeholder="Search for movies..."
                 iconPosition="left"
-                autosuggest={false}
+                autosuggest={true}
                 filterLabel="search"
+                enableRecentSuggestions={true}
+                enablePopularSuggestions={true}
+                enablePredictiveSuggestions={true}
+                popularSuggestionsConfig={{
+                  size: 3,
+                  minHits: 2,
+                  minChars: 4,
+                }}
+                recentSuggestionsConfig={{
+                  size: 3,
+                  minChars: 4,
+                }}
+                index="movie_app_final"
+                size={10}
               />
             </div>
-          </div>{" "}
-          <div className="left-bar">
-            <div>
-              {" "}
+          </div>
+          <div className="sub-container">
+            <div
+              className={
+                this.state.isClicked ? "left-bar-optional" : "left-bar"
+              }
+            >
               <div className="filter-heading center">
                 <b>
                   {" "}
@@ -96,7 +131,7 @@ class App extends Component {
                   label: "list-item",
                   input: "list-input",
                 }}
-              />{" "}
+              />
               <hr className="blue" />
               <div className="filter-heading center">
                 <b>
@@ -104,6 +139,7 @@ class App extends Component {
                   <i className="fa fa-dollar" /> Revenue{" "}
                 </b>
               </div>
+
               <SingleRange
                 componentId="revenue-list"
                 dataField="ran_revenue"
@@ -125,6 +161,7 @@ class App extends Component {
                 }}
               />
               <hr className="blue" />
+
               <div className="filter-heading center">
                 <b>
                   <i className="fa fa-star" /> Ratings
@@ -248,6 +285,7 @@ class App extends Component {
                 }}
               />
               <hr className="blue" />
+
               <div className="filter-heading center">
                 <b>
                   {" "}
@@ -260,6 +298,173 @@ class App extends Component {
                 className="datePicker"
               />
             </div>
+            <div
+              className={
+                this.state.isClicked
+                  ? "result-container-optional"
+                  : "result-container"
+              }
+            >
+              <SelectedFilters
+                showClearAll={true}
+                clearAllLabel="Clear filters"
+              />
+              <ReactiveList
+                defaultQuery={() => ({ track_total_hits: true })}
+                componentId="results"
+                dataField={[
+                  { field: "original_title", weight: 3 },
+                  { field: "original_title.search", weight: 2 },
+                ]}
+                react={{
+                  and: [
+                    "mainSearch",
+                    "RangeSlider",
+                    "language-list",
+                    "date-filter",
+                    "genres-list",
+                    "revenue-list",
+                  ],
+                }}
+                pagination={true}
+                className="Result_card"
+                paginationAt="bottom"
+                pages={5}
+                size={12}
+                Loader="Loading..."
+                noResults="No results were found..."
+                sortOptions={[
+                  {
+                    dataField: "revenue",
+                    sortBy: "desc",
+                    label: "Sort by Revenue(High to Low) \u00A0",
+                  },
+                  {
+                    dataField: "popularity",
+                    sortBy: "desc",
+                    label: "Sort by Popularity(High to Low)\u00A0 \u00A0",
+                  },
+                  {
+                    dataField: "vote_average",
+                    sortBy: "desc",
+                    label: "Sort by Ratings(High to Low) \u00A0",
+                  },
+                  {
+                    dataField: "original_title.raw",
+                    sortBy: "asc",
+                    label: "Sort by Title(A-Z) \u00A0",
+                  },
+                ]}
+                innerClass={{
+                  title: "result-title",
+                  listItem: "result-item",
+                  list: "list-container",
+                  sortOptions: "sort-options",
+                  resultStats: "result-stats",
+                  resultsInfo: "result-list-info",
+                  poweredBy: "powered-by",
+                }}
+              >
+                {({ data }) => (
+                  <ReactiveList.ResultCardsWrapper>
+                    {data.map((item) => (
+                      <div
+                        style={{ marginRight: "15px" }}
+                        className="main-description"
+                      >
+                        <div className="ih-item square effect6 top_to_bottom">
+                          <a
+                            target="#"
+                            href={"http://www.imdb.com/title/" + item.imdb_id}
+                          >
+                            <div className="img">
+                              <img
+                                src={
+                                  "https://image.tmdb.org/t/p/w500" +
+                                  item.poster_path
+                                }
+                                alt={item.original_title}
+                                className="result-image"
+                              />
+                            </div>
+                            <div className="info colored">
+                              <h3 className="overlay-title">
+                                {item.original_title}
+                              </h3>
+
+                              <div className="overlay-description">
+                                {item.tagline}
+                              </div>
+
+                              <div className="overlay-info">
+                                <div className="rating-time-score-container">
+                                  <div className="sub-title Rating-data">
+                                    <b>
+                                      Imdb
+                                      <span className="details">
+                                        {" "}
+                                        {item.vote_average}/10{" "}
+                                      </span>
+                                    </b>
+                                  </div>
+                                  <div className="time-data">
+                                    <b>
+                                      <span className="time">
+                                        <i className="fa fa-clock-o" />{" "}
+                                      </span>{" "}
+                                      <span className="details">
+                                        {item.time_str}
+                                      </span>
+                                    </b>
+                                  </div>
+                                  <div className="sub-title Score-data">
+                                    <b>
+                                      Score:
+                                      <span className="details">
+                                        {" "}
+                                        {item.score}
+                                      </span>
+                                    </b>
+                                  </div>
+                                </div>
+                                <div className="revenue-lang-container">
+                                  <div className="revenue-data">
+                                    <b>
+                                      <span>Revenue:</span>{" "}
+                                      <span className="details">
+                                        {" "}
+                                        ${item.or_revenue}
+                                      </span>{" "}
+                                    </b>
+                                  </div>
+
+                                  <div className="sub-title language-data">
+                                    <b>
+                                      Language:
+                                      <span className="details">
+                                        {" "}
+                                        {item.original_language}
+                                      </span>
+                                    </b>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </a>
+                        </div>
+                      </div>
+                    ))}
+                  </ReactiveList.ResultCardsWrapper>
+                )}
+              </ReactiveList>
+            </div>
+
+            <button
+              className="toggle-button"
+              onClick={this.handleClick.bind(this)}
+            >
+              {this.state.message}
+            </button>
           </div>
         </ReactiveBase>
       </div>
