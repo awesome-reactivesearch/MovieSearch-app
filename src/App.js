1b4140ff122d771ef68f990a8c0a5868b9831c52
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2022-03-02 22:02:26 +0530

    fix: search box issue

--- src/App.js
+++ src/App.js
@@ -25,7 +25,6 @@ class App extends Component {
               fontSize: "16px",
             },
             colors: {
-              textColor: "#fff",
               backgroundColor: "#212121",
               primaryTextColor: "#fff",
               primaryColor: "#2196F3",
@@ -47,8 +46,8 @@ class App extends Component {
             <div className="search-container">
               <SearchBox
                 componentId="mainSearch"
-                dataField={["original_title"]}
-                categoryField="title"
+                dataField={["original_title", "original_title.search"]}
+                categoryField="genres.keyword"
                 className="search-bar"
                 queryFormat="and"
                 placeholder="Search for movies..."
@@ -69,6 +68,7 @@ class App extends Component {
                 }}
                 index="movies-demo-app"
                 size={10}
+                innerClass={{ list: "list-class" }}
               />
             </div>
           </div>{" "}
